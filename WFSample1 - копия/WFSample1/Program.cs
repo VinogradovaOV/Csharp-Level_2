5ce029fe6d70a4c9cc4555f23f4d12000ed43b23
VinogradovaOV <ka_2197@mail.ru>
2018-10-21 21:09:25 +0300

    Домашняя работа №1 по курсу C#2

---
+++ WFSample1 - копия/WFSample1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WFSample1
+{
+    /// <summary>
+    /// Игра Астероиды, Виноградова О.В.
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Application.Run(new Form1());
+        }
+    }
+
+}
